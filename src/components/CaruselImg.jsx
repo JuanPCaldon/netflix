4c63603f492cba87a92bfe1729edbb4097048d41
JuanPCaldon <122814773+JuanPCaldon@users.noreply.github.com>
2024-03-20 21:36:32 -0500

    carousel3

--- src/components/CaruselImg.jsx
+++ src/components/CaruselImg.jsx
@@ -9,9 +9,10 @@ export const CaruselImg = () => {
 
   const [movieList,setMovieList ] = useState([]) 
   const [Isloading, setIsloading] = useState(true) 
+  const [Peliculas , setPeliculas] = useState(null)
   const Query = useQuery();
   const Search= Query.get("search");
-  console.log(Search);
+  //console.log(Search);
   
 
   const getmovie =(url) =>{
@@ -35,11 +36,20 @@ if(Isloading){
    return <Spiner/>
 } 
 
-console.log(movieList)
+const handleclick = (movie) =>{
+
+  setPeliculas( 
+     <MovieCard id={movie.id} titulo={movie.title} imagen={movie.poster_path} />
+  )
+   // 
+}
+
+//console.log(movieList)
   return (
               
     <div id="carouselExampleIndicators" class="carousel slide">
-    <div class={`${styles.indicatorCarousel} + carousel-indicators`}>
+    <div class={`carousel-indicators`}>
+       
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -47,58 +57,50 @@ console.log(movieList)
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
     </div>
     <div class="carousel-inner">
+        <h1 className={styles.TituloCarousel}> Popular en Netflix</h1>
           <div class="carousel-item active">
-            <div className='d-flex justify-content-center p-5'>
+            <div className={styles.tituloycarousel}>
               
-                  { movieList.map((movie, index) => ( 
-
-                        index < 6 && <img class= {`${styles.imgcarousel} + d-block w-100 m-1 rounded `}
-                                          src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                                          width={214}
-                                          height={121}
-                                          />
+                 <div className={'d-flex justify-content-center p-5'}>
+                      { movieList.map((movie, index) => ( 
+                        
+                           
+                            index < 6 && <img key={movie.id} class= {`${styles.imgcarousel} + d-block w-100 m-1 rounded `}
+                            src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                            width={230}
+                            height={345}
+                            onClick={()=>handleclick(movie)}
+                            
+                            />
                       
-                      ))    }  
+                        )) }  
+                  </div>
               </div>
           </div>
-    <div class="carousel-item">
-                <div className='d-flex justify-content-center p-5'>
-                    
-                    { movieList.map((movie, index) => ( 
-
-                          index < 6 && <img class= {`${styles.imgcarousel} + d-block w-100 m-1 rounded `}
-                                            src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                                            width={214}
-                                            height={121}
-                                            />
                         
-                        ))    }  
-              </div>
+      <div class="carousel-item">
+          
+      <div className={styles.tituloycarousel}>
+              
+              <div className={'d-flex justify-content-center p-5'}>
+                   { movieList.map((movie, index) => ( 
+                     
+                     index < 6 && <img class= {`${styles.imgcarousel} + d-block w-100 m-1 rounded `}
+                     src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                     width={214}
+                     height={121}
+                     />
+                     
+                     ))    }  
+               </div>
+           </div>
+       </div>  
     </div>
-    <div class="carousel-item">
-
-            <div className='d-flex justify-content-center p-5'>
-                
-                { movieList.map((movie, index) => ( 
-
-                      index < 6 && <img class= {`${styles.imgcarousel} + d-block w-100 m-1 rounded `}
-                                        src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                                        width={214}
-                                        height={121}
-                                        />
-                  
-                        ))    }  
-              </div>
-                  
-    </div>
-
-      
-    </div>
-    <button class={`${styles.previ} + carousel-control-prev`} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <button class={`${styles.prevbutton} + carousel-control-prev`} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class={`${styles.nexto} + carousel-control-next`} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <button class={`${styles.nextbutton} + carousel-control-next`} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
